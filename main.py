0675ba08484d8e3c0f6db6dda686980598494a4d
kihyunhee <pk97699@gmail.com>
2025-09-29 16:39:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import pandas as pd
+
+# CSV 파일 불러오기
+df = pd.read_csv("countriesMBTI_16types.csv")
+
+# 제목 표시
+st.title("Countries MBTI 16 Types 데이터 미리보기")
+
+# 상위 5줄만 출력
+st.write("데이터 상위 5줄:")
+st.dataframe(df.head())
 
